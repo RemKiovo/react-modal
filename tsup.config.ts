79f1a69c828837a1d8cf90182b2cd72962a29112
RemKiovo <epaulais.remi@gmail.com>
2024-10-20 19:55:39 +0200

    might work now

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  format: ['cjs', 'esm'],
+  format: ['cjs', 'esm', 'iife'],
   entry: ['src/index.ts'],
   dts: true,
   shims: true,
   skipNodeModulesBundle: true,
   clean: true,
+  sourcemap: true,
+  minify: true,
+  target: 'es2015',
+  external: ['react', 'react-dom', 'clsx', 'lucide-react'],
 })
